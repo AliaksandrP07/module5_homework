9046b6e4271ff275f714c061fff993ae55f6fe42
AliaksandrP07 <99407868+AliaksandrP07@users.noreply.github.com>
2022-03-18 13:14:14 +0300

    Add files via upload

---
+++ M5-HW-01.js
@@ -0,0 +1,11 @@
+let valueA= +prompt('Введите значение');
+console.log(typeof valueA);
+if (valueA===valueA) {
+if (typeof valueA==='number') {
+    if (valueA%2===0) {
+    console.log('Четное число')
+  } else {
+    console.log('Нечетное число')
+  }}} else {
+    console.log('Упс, кажется, вы ошиблись')
+  }
